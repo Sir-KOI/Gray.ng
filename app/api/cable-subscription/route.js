92e86e9e7a1f01f7e7db3a5347c5ece8cf8d4d4b
Sir-KOI <kehindeilekura@gmail.com>
2026-08-06 10:36:31 +0100

    remove: delete cable subscription route and page

--- app/api/cable-subscription/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function POST(req) {
-  return NextResponse.json({ 
-    success: false, 
-    message: 'cable subscription is temporarily under maintenance. Coming soon!' 
-  }, { status: 503 })
-}
